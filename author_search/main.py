aefcdefb3224eccf6c4105c50cc1f78f32ec9a14
Chris Laliwala Windows Desktop <claliwal@andrew.cmu.edu>
2024-04-14 21:05:57 -0400

    formatted everything with Black.

--- author_search/main.py
+++ author_search/main.py
@@ -1,8 +1,11 @@
 import click
 from .utils import get_works, get_h_index, make_author_list, sort_authors
 
-@click.command(help='author keywords. Pass up to 3 separated by spaces. Ex: author_search three keywords max')
-@click.argument('keywords', nargs=-1, type=click.STRING)
+
+@click.command(
+    help="author keywords. Pass up to 3 separated by spaces. Ex: author_search three keywords max"
+)
+@click.argument("keywords", nargs=-1, type=click.STRING)
 def main(keywords):
 
     # get works related to chosen keywords
@@ -14,7 +17,7 @@ def main(keywords):
     hindex_dict = dict.fromkeys(author_list)
     # make a dict mapping author to corresponding data from openAlex
     author_data_dict = dict.fromkeys(author_list)
-    
+
     for author in author_list:
         author_data, author_hindex = get_h_index(author)
         author_data_dict[author] = author_data
@@ -22,8 +25,11 @@ def main(keywords):
 
     # sort dictionary of authors by their h_index in descending order.
     sorted_hindex_dict = sort_authors(hindex_dict)
-    
+
     # print out authors with highest h-indices (top 5)
+    print("\n")
     for count, author in enumerate(sorted_hindex_dict):
+        display_name = author_data_dict[author]["display_name"]
+        hindex = sorted_hindex_dict[author]
         if count < 5:
-            print(f'{author_data_dict[author]['display_name']}, {author}, h-index: {sorted_hindex_dict[author]}')
+            print(f"{count+1}) {display_name}, orcid: {author}, h-index: {hindex}")
--- author_search/utils.py
+++ author_search/utils.py
@@ -2,35 +2,38 @@ import requests
 import time
 import sys
 
+
 def get_works(keywords):
     # make sure no more than 3 keywords have been passed.
     if len(keywords) > 3:
-        sys.exit('ERROR. More than three keywords have been chosen.')
+        sys.exit("ERROR. More than three keywords have been chosen.")
 
     # TEMPORARY
     word = keywords[0]
-    print('The keyword that will be used in the search is: ', word)
-    query = '+'.join(word.split())
+    print("The keyword that will be used in the search is: ", word)
+    query = "+".join(word.split())
 
-    url = f'https://api.openalex.org/works?filter=keywords.keyword:{query}'
+    url = f"https://api.openalex.org/works?filter=keywords.keyword:{query}"
     req = requests.get(url)
     data = req.json()
 
     return data
 
+
 def get_h_index(author):
     time.sleep(0.1)
-    url = f'https://api.openalex.org/authors/https://orcid.org/{author}'
+    url = f"https://api.openalex.org/authors/https://orcid.org/{author}"
     req = requests.get(url)
     data = req.json()
 
-    return data, data['summary_stats']['h_index']
+    return data, data["summary_stats"]["h_index"]
+
 
 def make_author_list(data):
     author_list = []
-    for work in data['results']:
-        for author in work['authorships']:
-            author_id = author['author']['orcid']
+    for work in data["results"]:
+        for author in work["authorships"]:
+            author_id = author["author"]["orcid"]
 
             # check to see if author is already added to list to avoid duplicates.
             # check to see if author has an orcid, if not, won't be added.
@@ -39,8 +42,11 @@ def make_author_list(data):
 
     return author_list
 
+
 def sort_authors(hindex_dict):
     # sort authors by h_index in descending order.
-    sorted_h_index_dict = dict(sorted(hindex_dict.items(), key=lambda x:x[1], reverse=True))
+    sorted_h_index_dict = dict(
+        sorted(hindex_dict.items(), key=lambda x: x[1], reverse=True)
+    )
 
     return sorted_h_index_dict
