d6fc7929b8ac2dde23cd924d3726160627bbdc4d
Chris Laliwala Windows Desktop <claliwal@andrew.cmu.edu>
2024-04-14 20:19:45 -0400

    The base code for the package. The package has limited capabilities: only takes 1 keyword, returns 5 authors with highest h-index and a link to their orcid. Package can also be easily broken, isn't formatted with Blackm doesn't follow Pep8, and has not tests.

---
+++ author_search/main.py
@@ -0,0 +1,29 @@
+import click
+from .utils import get_works, get_h_index, make_author_list, sort_authors
+
+@click.command(help='author keywords. Pass up to 3 separated by spaces. Ex: author_search three keywords max')
+@click.argument('keywords', nargs=-1, type=click.STRING)
+def main(keywords):
+
+    # get works related to chosen keywords
+    data = get_works(keywords)
+    # make a list of authors associated with those works
+    author_list = make_author_list(data)
+
+    # make a dict mapping author to corresponding h-index
+    hindex_dict = dict.fromkeys(author_list)
+    # make a dict mapping author to corresponding data from openAlex
+    author_data_dict = dict.fromkeys(author_list)
+    
+    for author in author_list:
+        author_data, author_hindex = get_h_index(author)
+        author_data_dict[author] = author_data
+        hindex_dict[author] = author_hindex
+
+    # sort dictionary of authors by their h_index in descending order.
+    sorted_hindex_dict = sort_authors(hindex_dict)
+    
+    # print out authors with highest h-indices (top 5)
+    for count, author in enumerate(sorted_hindex_dict):
+        if count < 5:
+            print(f'{author_data_dict[author]['display_name']}, {author}, h-index: {sorted_hindex_dict[author]}')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='author_search',
+      version='0.0.1',
+      description='author search utility command line utility',
+      maintainer='Chris Laliwala',
+      maintainer_email='claliwal@andrew.cmu.edu',
+      license='MIT',
+      packages=['author_search'],
+      entry_points={'console_scripts': ['author_search = author_search.main:main']},
+      long_descript='''\
+author command line utility
+============================''')
